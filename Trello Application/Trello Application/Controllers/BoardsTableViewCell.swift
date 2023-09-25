c4d2bdf5a1126ae37413ddae1f225e4611e5dacc
Liene <liene.krista@gmail.com>
2023-09-25 05:35:55 +0300

    Updated main view, added table list

---
+++ Trello Application/Trello Application/Controllers/BoardsTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  BoardsTableViewCell.swift
+//  Trello Application
+//
+//  Created by liene.krista.neimane on 25/09/2023.
+//
+
+import UIKit
+
+class BoardsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var boardLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
